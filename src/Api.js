8c426175d52affa193157daaafaafeaaa6923f1e
Leni <lenard.mate.98@gmail.com>
2021-04-28 14:59:44 +0300

    API

---
+++ src/Api.js
@@ -0,0 +1,70 @@
+import React from 'react';
+//import axios from 'axios';
+/**
+ * App
+ *
+ * Simple react js fetch example
+ */
+class Forex extends React.Component {
+
+    /**
+     * constructor
+     *
+     * @object  @props  parent props
+     * @object  @state  component state
+     */
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            items: [],
+            isLoaded: false
+        }
+
+    }
+
+    /**
+     * componentDidMount
+     *
+     * Fetch json array of objects from given url and update state.
+     */
+    componentDidMount() {
+//https://jsonplaceholder.typicode.com/users
+        fetch('https://api.agify.io/?name=bella')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    items: json,
+                    isLoaded: true, 
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
+
+    }
+
+    /**
+     * render
+     *
+     * Render UI
+     */
+    render() {
+
+        const { isLoaded, items } = this.state;
+        console.log(this.state.items);
+   
+        if (!isLoaded)
+            return <div>Loading...</div>;
+
+        return (
+            <div className="Forex">
+            {this.state.items.name} {this.state.items.age} {this.state.items.count}
+            </div>
+        );
+
+    }
+
+}
+
+export default Forex;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Forex from "./Api"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My name is mate and i want to lear react js , thank you for your cooperation
+          My name is mate and i want to learn react js , thank you for your cooperation
+         
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Forex/>
       </header>
+     
     </div>
   );
 }
+
+
 
 export default App;
